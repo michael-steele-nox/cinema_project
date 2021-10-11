1f5521f027be3852ee3d909476553dd545c485c3
michael-steele-nox <48181422+michael-steele-nox@users.noreply.github.com>
2021-10-11 23:41:54 +0300

    Implement responsive layout
    Implement responsive layout

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,8 @@
 const schemeSvg = document.querySelector('.scheme-svg');
 const totalPriceTag = document.querySelector('.price-total');
+const menuButton = document.querySelector(".m-menu");
+const menu = document.querySelector(".menu");
+
 let cost = 800;
 let totalPrice = 0;
 schemeSvg.addEventListener("click", (event) => {
@@ -11,4 +14,8 @@ schemeSvg.addEventListener("click", (event) => {
 		totalPriceTag.textContent = totalPrice;
 		console.log()
 	}
+});
+
+menuButton.addEventListener('click', () => {
+	menu.classList.toggle("is-open");
 });
